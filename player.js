4db4598866270d0ec872abc4d99d550b6132bf90
TheLegendOfThePugs <j.gobbett@internode.on.net>
2016-04-06 18:09:03 +0930

    platformer
    for AIE

---
+++ player.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var Player = function() {
+    this.image = document.createElement("img")
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
+    this.width = 159;
+    this.height = 163;
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.angularVelocity = 0;
+    this.rotation = 0;
+    this.image.src ="hero.png"
+};
+
+
+
+var player = new Player();
+/*
+Player.prototype.update = function(deltaTime)
+{
+    if( typepof(this.rotation) == "undefined" )
+        this.rotation = 0;
+    this.rotation += deltaTime;
+}*/
+
+Player.prototype.draw = function()
+{
+    context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.rotation);
+        context.drawImage(this.image, -this.width/2, -this.height/2);
+    context.restore();
+}
+
+
+
