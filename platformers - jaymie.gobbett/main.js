10d7c7fd29b714fd3dabc570f12924838c845ac1
TheLegendOfThePugs <j.gobbett@internode.on.net>
2016-04-06 19:03:17 +0930

    adding some stuff - 7:03

--- platformers - jaymie.gobbett/main.js
+++ platformers - jaymie.gobbett/main.js
@@ -7,7 +7,7 @@ var startFrameMillis = Date.now();
 var endFrameMillis = Date.now();
 
 var player = new Player();
-var keyboard = new Keyboard();
+//var keyboard = new Keyboard();
 
 // This function will return the time in seconds since the function 
 // was last called
@@ -24,7 +24,22 @@ var TILESET_COUNT_Y = 14;
 var tileset = document.createElement("img")
 tileset.src = "tileset.png"
 
-
+function drawMap() {
+    for (var layeridx = 0; layeridx < LAYER_COUNT; layeridx++) {
+        var idx = 0;
+        for (var y = 0; y < level1.layers[layeridx].height; y++) {
+            for (var x = 0; x < level1.layers[layeridx].width; y++) {
+                if (level1.layers[layeridx].data[idx] != 0) {
+                    var tileIndex = level1.layers[layeridx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
+                }
+                idx++;
+            }
+        }
+    }
+}
 
 function getDeltaTime()
 {
@@ -68,9 +83,11 @@ function run()
 	context.fillStyle = "#ccc";		
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
+    drawMap
+    
 	var deltaTime = getDeltaTime();
 	
-    player.update(deltaTime);
+    //player.update(deltaTime);
     player.draw();
     
 	context.drawImage(chuckNorris, SCREEN_WIDTH/2 - chuckNorris.width/2, SCREEN_HEIGHT/2 - chuckNorris.height/2);
@@ -86,34 +103,16 @@ function run()
 		fps = fpsCount;
 		fpsCount = 0;
 	}		
-		
+	
 	// draw the FPS
 	context.fillStyle = "#f00";
 	context.font="14px Arial";
 	context.fillText("FPS: " + fps, 5, 20, 100);
 }
 
-function drawMap()
-{
-    for(var layeridx=0; layeridx<LATER_COUNT;layeridx++)
-    {
-        var idx = 0;
-        for(var y = 0; y < level1.layers[layeridx].height; y++)
-        {
-            for(var x = 0; x < level1.layers[layeridx].width; y++)
-            {
-                if( level1.layers[layeridx].data[idx] != 0)
-                {
-                    var tileIndex = level1.layers[layeridx].data[idx]-1;
-                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X)*(TILE_SPACING);
-                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y))*(TILESET_TILE + TILESET_SPACING);
-                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
-                }
-                idx++;
-            }
-        }
-    }
-}
+
+
+
 
 //-------------------- Don't modify anything below here
 
