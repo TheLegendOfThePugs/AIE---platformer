d7d6555617d4791b37c28a315bc70be6c7bfd627
TheLegendOfThePugs <j.gobbett@internode.on.net>
2016-04-06 19:51:29 +0930

    magor fixes - 7:51pm

--- platformers - jaymie.gobbett/main.js
+++ platformers - jaymie.gobbett/main.js
@@ -29,12 +29,12 @@ function drawMap() {
         var idx = 0;
         for (var y = 0; y < level1.layers[layerIdx].height; y++)
          {
-            for (var x = 0; x < level1.layers[layerIdx].width; y++)
+            for (var x = 0; x < level1.layers[layerIdx].width; x++)
              {
                 if (level1.layers[layerIdx].data[idx] != 0)
                  {
                     var tileIndex = level1.layers[layerIdx].data[idx] - 1;
-                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_SPACING);
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
                     var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
                     context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
                 }
