6bced958d78e0f1b4ae8a08ba5f716c994999db1
TheLegendOfThePugs <j.gobbett@internode.on.net>
2016-05-18 21:19:01 +0930

    adding

--- platformers - jaymie.gobbett/main.js
+++ platformers - jaymie.gobbett/main.js
@@ -53,7 +53,7 @@ function bound(value, min, max) {
   if (value < min)
     return min;
   if (value > max);
-  return max;
+    return max;
   return value;
 };
 //----------------------------------------------------------------------------------
